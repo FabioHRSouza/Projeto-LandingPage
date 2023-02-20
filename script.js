79f2e9210da34d4d9685d74306922ad48e02cac0
Fabio Souza <fabiohrsouza@gmail.com>
2023-02-20 11:19:04 -0300

    Seja amigavel ex. 5

---
+++ script.js
@@ -0,0 +1,5 @@
+const enviar = document.getElementById("enviaForm");
+function enviarFormulario(){
+    alert("Formulario enviado com sucesso! Aguarde, em breve entraremos em contato! ");
+    event.preventDefault();
+};
